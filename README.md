71c084ee176013e517a0f66d6e5551b1396f485f
jiasir <jiasir@icloud.com>
2023-11-16 16:49:05 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # openai-api-proxy
-A Proxy for OpenAI API
+A Proxy for OpenAI API. This app will proxy https://your.domain.com/v1/chat/completions to https://api.openai.com/v1/chat/completions
+
+#### How to use
+* Clone the repo to your local machine
+* Install the dependencies
+  * `gradlew build`
+* Set the system environment to your server and run the application
+  * `export OPENAI_API_KEY=<your-openai-api-key>`
+  * `java -jar build/libs/openai-api-proxy-0.0.1-SNAPSHOT.jar`
+
+#### POST test
+```bash
+curl https://api.openai.com/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $OPENAI_API_KEY" \
+  -d '{
+    "model": "gpt-3.5-turbo",
+    "messages": [
+      {
+        "role": "system",
+        "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."
+      },
+      {
+        "role": "user",
+        "content": "Compose a poem that explains the concept of recursion in programming."
+      }
+    ]
+  }'
---
+++ src/main/java/dev/sunsun/proxy/OpenAI.java
@@ -0,0 +1,40 @@
+/**
+ * This class represents a REST controller that handles requests to the OpenAI API.
+ * The API key is retrieved from the system environment variable "OPENAI_API_KEY".
+ * The "getOpenAiResponse" method sends a POST request to the OpenAI API with a prompt message and returns the response.
+ */
+package dev.sunsun.proxy;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class OpenAI {
+
+    // gpt-4 (and gpt-4 turbo), gpt-3.5-turbo
+     private static final String OPENAI_API_URL = "https://api.openai.com/v1/chat/completions";
+
+    /**
+     * Sends a message to OpenAI API and returns the response as a String.
+     *
+     * @param message the message is a JSON to send to the OpenAI API
+     * @return the response from the OpenAI API as a String
+     */
+    @PostMapping("/v1/chat/completions")
+    public String getOpenAiResponse(@RequestBody Model message) {
+        RestTemplate restTemplate = new RestTemplate();
+        Model requestBody = new Model("gpt-4", message.messages());
+        String authorizationHeader = "Bearer " + System.getenv("OPENAI_API_KEY");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", authorizationHeader);
+        HttpEntity<Model> request = new HttpEntity<>(requestBody, headers);
+        ResponseEntity<String> response = restTemplate.postForEntity(OPENAI_API_URL, request, String.class);
+        return response.getBody();
+    }
+}
